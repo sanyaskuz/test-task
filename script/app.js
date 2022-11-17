29aa8c05b0caa95a28c5a9000a21b1bd9a19edf7
sanyaskuz <sanya.skuz@gmail.com>
2022-11-17 23:01:36 +0200

    end-map

--- script/app.js
+++ script/app.js
@@ -1,3 +1,4 @@
+// Castom Selector
 function select() {
   let selectHeader = document.querySelectorAll('.select__header');
   let selectItem = document.querySelectorAll('.select__item');
@@ -26,93 +27,127 @@ function select() {
 };
 
 select();
+// 
 
+//Get Map
 
 function initMap() {
   const uluru = { lat: 43.4558941, lng: -80.5307556 };
-  const styledMapType = new google.maps.StyledMapType(
-    [
-      
-        {
-          "featureType": "all",
-          "stylers": [
-            { "color": "#fcfaf6" }
-          ]
-        },
-        {
-          "featureType": "road",
-          "stylers": [
-            { "color": "#ffffff" }
-          ]
-        },
-        {
-          "featureType": "geometry.labels",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            { "color": "#dddcdb" }
-          ]
-        },
-        {
-          "featureType": "landscape.natural.landcover",
-          "stylers": [
-            { "color": "#e0f2e5" }
-          ]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "labels",
-          "stylers": [
-            { "visibility": "off" }
-          ]
-        },
-        {
-          "featureType": "poi.park",
-          "stylers": [
-            { "color": "#e0f2e5" }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "labels.icon",
-          "stylers": [
-            {"color": "#8cb9f4"}
-          ]
-        },
-        {
-          "featureType": "transit.station.bus",
-          "stylers": [
-            {"color": "#8cb9f4"}
-          ]
-        },
-        {
-          "featureType": "water",
-          "stylers": [
-            { "color": "#d6f0fa" }
-          ]
-        },
-        
-      
-    ],
-    { name: "Styled Map" }
-  );
-
+  
 
 
   const map = new google.maps.Map(document.querySelector(".map"), {
     center: uluru,
     zoom: 14,
-    mapTypeControlOptions: {
-      mapTypeIds: [],
-    },
+    mapTypeControl: false,
+    styles:[
+      
+      {
+        elementType: "geometry",
+        stylers: [
+          { color: "#fcfaf6" }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [
+          { color: "#ffffff" }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {color: "#ffffff"}
+        ]
+      },
+        {
+          elementType: "labels.text.fill",
+          stylers: [
+            { color: "#dddcdb" }
+          ]
+        },
+      {
+        featureType: "landscape.natural.landcover",
+        elementType: "geometry",
+        stylers: [
+          { color: "#e0f2e5" }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [{ color: "#fcfaf6" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          { color: "#d6d6d6"}
+        ]
+      },
+
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          { color: "#e0f2e5" }
+        ]
+      },
+      {
+        featureType: "poi",
+        stylers: [{ visibility: "off" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#8f7d77" }],
+      },
+      {
+        featureType: "transit.line",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#ebe3cd" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "geometry",
+        stylers: [{ color: "#dfd2ae" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.icon",
+        stylers: [{ color:""}],
+      },
+      {
+        featureType: "water",
+        stylers: [
+          { color: "#d6f0fa" }
+        ]
+      },
+      
     
+  ]
 
   });
-  const marker = new google.maps.Marker({
+
+  const marker = new google.maps.Marker ({
     position: uluru,
+    icon: "./img/Vector.png",
     map: map,
   });
-  map.mapTypes.set("styled_map", styledMapType);
-  map.setMapTypeId("styled_map");
+  const infoMarker = new google.maps.InfoWindow({
+    content: 
+    '<div><h2>Voodoo</h2><p>137 Glasgow St., Unit 115<br> Kitchener, ON N2G 4X8 <br>Ukraine</p><div style="margin-left:5px;"><div style="align-items: center;display: flex; "><div style="margin: 0px 9px;"> &#128222;</div><div style="opacity: 0.5;"> 1-800-480-9597</div></div><div style="align-items: center;display: flex;"><div style=" font-size: 20px; margin: 0px 9px;">&#9993;</div><div style="opacity: 0.5;">info@voodoo.com</div></div></div>'
+  });
+  infoMarker.open(map, marker);   
+
 }
 
 window.initMap = initMap;
+//
